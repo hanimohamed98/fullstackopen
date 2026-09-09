5cf102d311c114d1177a509ee19d9da9f0a9f68a
hani <hani@Mac.home>
2026-09-09 22:39:12 +0300

    complete unicafe exercises 1.6 to 1.10

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+    
+  return (
+
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+
+  }
+
+const StatisticLine = (props) => {
+
+  return (
+
+    <p>{props.text} {props.value}</p>
+
+  )
+}
+
+
+const Statistics = (props) => {
+
+  if (props.all === 0) {
+    
+  return (
+
+    <div> 
+      No feedback given
+    </div>
+
+  )
+
+  }
+
+  return (
+
+    <div>
+
+    <StatisticLine text = "good" value = {props.good}/>
+    <StatisticLine text = "neutral" value = {props.neutral}/>
+    <StatisticLine text = "bad" value = {props.bad}/>
+    <StatisticLine text = "all" value = {props.all}/>
+    <StatisticLine text = "average" value = {props.average}/>
+    <StatisticLine text = "positive" value = {props.positive + ' %'}/>
+
+
+    </div>
+
+  )
+
+
+}
+
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+  
+  
+  return (
+    <div>
+      
+    <h1>give feedback</h1>
+  
+    <Button onClick={() => setGood(good+1)} text="good" />
+    <Button onClick={() => setNeutral(neutral+1)} text="neutral" />
+    <Button onClick={() => setBad(bad+1)} text="bad" />
+
+    <h1>statistics</h1>
+
+    <Statistics
+
+    good={good}
+    neutral={neutral}
+    bad={bad}
+    all={all}
+    average={average}
+    positive={positive} 
+
+
+    />
+  </div>
+  )
+}
+
+export default App
