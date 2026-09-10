0a2ade89a2096f71a9a54f40d945d491a613cd2c
hani <hani@JayRay-MacBook-Air.local>
2026-09-10 18:27:07 +0300

    complete phonebook exercise 2.13

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const PersonForm = (props) => {
   return (
@@ -41,8 +41,8 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
 
     useEffect (() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -63,8 +63,8 @@ const addName = (event) => {
   }
 
 
-  axios
-  .post('http://localhost:3001/persons', personObject)
+  personService
+  .create(personObject)
   .then(response => {
     setPersons(persons.concat(response.data))
     setNewName('')
