ad82b74feea23dd15c398152db54d44ec82ee015
hani <hani@JayRay-MacBook-Air.local>
2026-09-10 18:09:03 +0300

    complete phonebook exercise 2.12

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -62,9 +62,14 @@ const addName = (event) => {
     number: newNumber
   }
 
-  setPersons(persons.concat(personObject))
-  setNewName('')
-  setNewNumber('')
+
+  axios
+  .post('http://localhost:3001/persons', personObject)
+  .then(response => {
+    setPersons(persons.concat(response.data))
+    setNewName('')
+    setNewNumber('')
+  })
 
 }
 
