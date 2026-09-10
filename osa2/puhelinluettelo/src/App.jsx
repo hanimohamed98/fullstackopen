0bbce4622ea596902afcde57e9631595ed8451d5
hani <hani@JayRay-MacBook-Air.local>
2026-09-10 17:28:24 +0300

    complete phonebook exercise 2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const PersonForm = (props) => {
   return (
@@ -35,11 +36,17 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+    useEffect (() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
 const addName = (event) => {
   event.preventDefault()
@@ -94,7 +101,6 @@ return (
     </div>
   )
 }
-
 
 
 export default App
