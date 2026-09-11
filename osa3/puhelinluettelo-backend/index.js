b3c865a5699ec253c42141ab074007337343fbbc
hani <hani@Mac.home>
2026-09-11 17:51:07 +0300

    complete phonebook backend exercise 3.8

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (request) => {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
