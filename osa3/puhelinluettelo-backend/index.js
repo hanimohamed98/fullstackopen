1f2012046648f817194445c9e4d88e908321e8ff
hani <hani@Mac.home>
2026-09-12 17:15:43 +0300

    complete phonebook database exercise 3.13

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
-const cors = require ('cors')
+const cors = require('cors')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.json())
@@ -42,7 +46,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
     response.json(persons)
+    })
 })
 
 app.get ('/api/persons/:id', (request, response) => {
