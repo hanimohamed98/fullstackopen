7e175faddb2a1c6bec97c9d66ae6555fdfa134fc
hani <hani@JayRay-MacBook-Air.local>
2026-09-14 13:42:09 +0300

    complete total likes exercise 4.4

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -1,5 +1,6 @@
-const { test} = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
+
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
@@ -7,4 +8,28 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      title: 'Eka blogi',
+      author: 'Hani',
+      url: 'https://example.com',
+      likes: 5,
+
+    },
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+    test('when list is empty, returns zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
 })
