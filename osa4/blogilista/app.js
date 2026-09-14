5cf6d166f8e430ffddadf69c31d420dcaa893fa6
hani <hani@JayRay-MacBook-Air.local>
2026-09-14 12:38:00 +0300

    refactor bloglist structure for exercise 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dns = require('dns')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+
+const app = express()
+
+dns.setServers(['8.8.8.8', '1.1.1.1'])
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,47 +1,6 @@
-require('dotenv').config()
+const app = require('./app') 
+const config = require('./utils/config')
 
-const express = require('express')
-const mongoose = require('mongoose')
-const dns = require('dns')
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-dns.setServers(['8.8.8.8', '1.1.1.1'])
-mongoose.connect(mongoUrl, { family: 4 })
-.then(() => {
-    console.log('connected to MongoDB')
-})
-.catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-})
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
