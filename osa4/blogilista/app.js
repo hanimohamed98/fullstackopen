24380c1d5b360560834bb135b75efb49bee5888d
hani <hani@Mac.home>
2026-09-15 15:05:01 +0300

    complete login exercise 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,6 +4,7 @@ const dns = require('dns')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -22,6 +23,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 module.exports = app
