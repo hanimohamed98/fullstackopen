b4584183095df4065bb33c5061a70e5b2f56ae61
hani <hani@Mac.home>
2026-09-15 15:47:28 +0300

    complete token blog creation exercise 4.19

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -25,5 +25,15 @@ app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    next (error)
+}
+
+app.use(errorHandler)
 
 module.exports = app
+
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhhbmkiLCJpZCI6IjZhYTgzODI1MjdmZTZhYTg4YWU0NmI5YSIsImlhdCI6MTc4OTQ3NTQ2MH0._Odqs-NPSH17LJ1RnzwFunDjXJFQcqSE9-2V8cXo9Xc
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,16 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -15,7 +25,14 @@ blogsRouter.post('/', async (request, response) => {
 
     const body = request.body
 
-    const user = await User.findOne({})
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
