4372d1c352e2e8fcab4d6f3d5e60c5070665f35a
hani <hani@Mac.home>
2026-09-16 19:05:27 +0300

    complete blog details exercise 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
+import { useState } from 'react'
+
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  
+  if (showDetails) {
+    return (
+    <div>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(false)}>hide</button>
+      </div>
+
+      <div>{blog.url}</div>
+
+      <div>
+        likes {blog.likes}
+        <button>like</button>
+      </div>
+
+      <div>{blog.user && blog.user.name}</div>
+    </div>
+    )
+  }
+  
+  
+  return (
   <div>
     {blog.title} {blog.author}
+    <button onClick={() => setShowDetails(true)}>
+      view
+    </button>
   </div>  
 )
 
+}
 export default Blog
